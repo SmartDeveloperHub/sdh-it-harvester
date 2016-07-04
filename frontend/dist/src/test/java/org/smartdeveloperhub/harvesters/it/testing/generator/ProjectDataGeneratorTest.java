952ee0495916428782f03f983e8cb0fb54a8b089
Miguel Esteban Gutiérrez <m.esteban.gutierrez@gmail.com>
2016-07-04 15:16:17 +0200

    Technical debt

---
+++ frontend/dist/src/test/java/org/smartdeveloperhub/harvesters/it/testing/generator/ProjectDataGeneratorTest.java
@@ -0,0 +1,55 @@
+/**
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   This file is part of the Smart Developer Hub Project:
+ *     http://www.smartdeveloperhub.org/
+ *
+ *   Center for Open Middleware
+ *     http://www.centeropenmiddleware.com/
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Copyright (C) 2015-2016 Center for Open Middleware.
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Artifact    : org.smartdeveloperhub.harvesters.it.frontend:it-frontend-dist:0.1.0-SNAPSHOT
+ *   Bundle      : it-frontend-dist-0.1.0-SNAPSHOT.war
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ */
+package org.smartdeveloperhub.harvesters.it.testing.generator;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.io.Files;
+
+public class ProjectDataGeneratorTest {
+
+	private static final Logger LOGGER=LoggerFactory.getLogger(ProjectDataGenerator.class);
+
+	@Rule
+	public TemporaryFolder util=new TemporaryFolder();
+
+	@Test
+	public void testMain() throws Exception {
+		final File file = this.util.newFile();
+		ProjectDataGenerator.main(file.toString());
+		final String content = Files.toString(file, StandardCharsets.UTF_8);
+		LOGGER.trace("Serialization:\n{}",content);
+	}
+
+}
